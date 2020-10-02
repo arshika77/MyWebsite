929ec2293ecf1c8eef7df7889845f4140164dc67
Arshika Lalan <52136458+arshika77@users.noreply.github.com>
2020-10-02 15:14:52 +0530

    Updates

---
+++ precache-manifest.826a7cf1ba2c9d7dfae900add5179a18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42d43ebcc2f0855c1d1a2db3aa121595",
+    "url": "/www.lalanarshika.com/index.html"
+  },
+  {
+    "revision": "34378cd6309104c85712",
+    "url": "/www.lalanarshika.com/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "8450b0164ab6c4010de5",
+    "url": "/www.lalanarshika.com/static/js/2.2205d9a3.chunk.js"
+  },
+  {
+    "revision": "e980a7f3cee8e03adfb625e54adfa237",
+    "url": "/www.lalanarshika.com/static/js/2.2205d9a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34378cd6309104c85712",
+    "url": "/www.lalanarshika.com/static/js/main.969832fb.chunk.js"
+  },
+  {
+    "revision": "7d8c2e28d34241d3fc64",
+    "url": "/www.lalanarshika.com/static/js/runtime-main.c7d3c149.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MyWebsite/precache-manifest.ad806df5cd6f08534f83d99b40478781.js"
+  "/www.lalanarshika.com/precache-manifest.826a7cf1ba2c9d7dfae900add5179a18.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MyWebsite/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/www.lalanarshika.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
