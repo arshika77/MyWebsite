44dd89732501d1b6590c09334ac98a2f76927615
Arshika Lalan <52136458+arshika77@users.noreply.github.com>
2020-10-03 15:17:55 +0530

    Updates

--- precache-manifest.b255df0c6dc6ab9e40f2d3a9cf19e51a.js
+++ precache-manifest.b255df0c6dc6ab9e40f2d3a9cf19e51a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5f4b002961d280ca2d1957095fcc655f",
+    "revision": "3a05b20ebf79d678991a4916483e078b",
     "url": "/MyWebsite/index.html"
   },
   {
-    "revision": "31ffd6030ebc39735186",
+    "revision": "88ea133dbea6bb218e76",
     "url": "/MyWebsite/static/css/main.5facb584.chunk.css"
   },
   {
-    "revision": "8450b0164ab6c4010de5",
-    "url": "/MyWebsite/static/js/2.2205d9a3.chunk.js"
+    "revision": "06684fabe57954ed266a",
+    "url": "/MyWebsite/static/js/2.c81adea6.chunk.js"
   },
   {
     "revision": "e980a7f3cee8e03adfb625e54adfa237",
-    "url": "/MyWebsite/static/js/2.2205d9a3.chunk.js.LICENSE.txt"
+    "url": "/MyWebsite/static/js/2.c81adea6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "31ffd6030ebc39735186",
-    "url": "/MyWebsite/static/js/main.2b2300ff.chunk.js"
+    "revision": "88ea133dbea6bb218e76",
+    "url": "/MyWebsite/static/js/main.daa61803.chunk.js"
   },
   {
     "revision": "671bd5fa3146e2a2892c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MyWebsite/precache-manifest.24e98511251b2b9f251293402f82c4a2.js"
+  "/MyWebsite/precache-manifest.b255df0c6dc6ab9e40f2d3a9cf19e51a.js"
 );
 
 self.addEventListener('message', (event) => {
