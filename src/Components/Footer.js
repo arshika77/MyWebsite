9b72fbfddb1d31358682bbf62744ddd80a8a8075
Arshika Lalan <52136458+arshika77@users.noreply.github.com>
2020-10-03 17:37:26 +0530

    Footer fixes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
 
     if(this.props.data){
       var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+        return <li key={network.name}><a href={network.url} target="_blank" rel="noopener noreferrer"><i className={network.className}></i></a></li>
       })
     }
 
@@ -14,13 +14,12 @@ class Footer extends Component {
 
      <div className="row">
         <div className="twelve columns">
-           <ul className="social-links" target="_blank" rel="noopener noreferrer">
+           <ul className="social-links">
               {networks}
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright Arshika Lalan</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li><a href="#footer">&copy; Copyright Arshika Lalan</a></li>
            </ul>
 
         </div>
